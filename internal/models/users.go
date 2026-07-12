e7b0647f7c099fa31442ce94ee083211f8939724
muskiteer <agarwal.vansh3110@gmail.com>
2026-07-12 22:50:18 +0530

    Changing maria to mysql

--- internal/models/users.go
+++ internal/models/users.go
@@ -34,7 +34,7 @@ func (m *UserModel) Insert(name,email,password string) error{
 	if err != nil{
 		var mySQLError *mysql.MySQLError
 		if errors.As(err,&mySQLError){
-			if mySQLError.Number==1062 && strings.Contains(mySQLError.Message,"users_uc_email"){
+			if mySQLError.Number==1062{
 				return ErrDuplicateEmaiil
 			}
 		}
